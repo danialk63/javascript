5e497448dc47d3acd05092da2c45605ea81ab724
Ahmad Khalid <danialkhalid63@gmail.com>
2021-10-04 17:17:29 +0500

    javaScript
    java Script practice

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("Helllooooo");
+console.warn("This is warning");
+console.error("This is error");
+
+var a = 10;
+var b = 20;
+console.log(a+b);
+
+var num1 = 100;
+var num2 = 200;
+
+var str = "This is string";
+var str2 = 'this is also a string';
+
+var marks = {
+    ali: 80,
+    ahmad: 90,
+    ammar:88
+};
+console.log(marks);
+
+
+
+var arr = [1,2,3,'ali',4,5];
+
+
+
+
+
+function avg(a,b){
+
+    return (a + b)/2;
+}
+c = avg(100,10);
+console.log(c);
+
+
+var age = 30;
+if(age>18){
+
+    console.log("You can drive a car")
+}
+else{
+    console.log("You cant drive");
+}
